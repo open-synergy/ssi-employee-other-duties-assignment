f1d150ee772fa16a453e6d39c6be52745c90203b
Andhitia Rama <andhitia.r@gmail.com>
2026-07-17 09:22:54 +0700

    [ADD] ssi_employee_other_duties_assignment_operating_unit
    Modul glue baru yang menambahkan operating_unit_id (mixin
    mixin.single_operating_unit) pada dokumen employee_other_duties_assignment,
    terisi otomatis dari operating unit karyawan lewat onchange employee_id.
    
    * Field operating_unit_id tampil di tree/form/search (setelah company_id/
      employee_id), hanya untuk grup operating_unit.group_multi_operating_unit,
      plus group-by "Operating Unit" pada search view.
    * Grup Operating Unit ditambahkan pada kategori data-ownership modul basis;
      grup Company basis meng-imply grup ini dan diberi akses root + admin.
    * Record rule membatasi dokumen sesuai user.operating_unit_ids.
    * Unit test skenario YAML (odoo-yaml-test): onchange set OU, ganti employee
      mengganti OU, employee dikosongkan mengosongkan OU, dan OU bertahan
      melewati alur confirm -> approve -> done.

---
+++ ssi_employee_other_duties_assignment_operating_unit/tests/test_employee_other_duties_assignment_operating_unit.py
@@ -0,0 +1,15 @@
+# Copyright 2026 OpenSynergy Indonesia
+# Copyright 2026 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo.tests import tagged
+
+from odoo_yaml_test import YamlTransactionCase
+
+
+@tagged("post_install", "-at_install")
+class TestEmployeeOtherDutiesAssignmentOperatingUnit(YamlTransactionCase):
+    def test_employee_other_duties_assignment_operating_unit(self):
+        self.run_yaml_scenario(
+            "test_data_employee_other_duties_assignment_operating_unit.yaml"
+        )
